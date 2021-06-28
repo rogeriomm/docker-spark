6ed158905eb9755c387a4a0b19842807e85cf8ab
Rogerio Machado <rogermm@gmail.com>
2021-06-28 17:04:18 -0300

    Update

--- build.sh
+++ build.sh
@@ -2,21 +2,38 @@
 
 set -e
 
-TAG=3.1.1-hadoop3.2-java11
+TAG=$(git rev-parse --abbrev-ref HEAD)
 
 build() {
-    NAME=$1
-    IMAGE=bde2020/spark-$NAME:$TAG
-    cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
+    USER=$1
+    PREFIX=$2
+    NAME=$3
+    IMAGE=$USER/$PREFIX-$NAME:$TAG
+    cd $([ -z "$4" ] && echo "./$NAME" || echo "$4")
     echo '--------------------------' building $IMAGE in $(pwd)
-    docker build -t $IMAGE .
+    echo "--->docker build -t $IMAGE --build-arg USERNAME=$1 --build-arg TAG=$TAG .<---"
+    #docker build --no-cache -t $IMAGE --build-arg USERNAME=$1 --build-arg TAG=$TAG .
+    docker build -t $IMAGE --build-arg USERNAME=$1 --build-arg TAG=$TAG .
     cd -
 }
 
-build base
-build master
-build worker
-build submit
-build java-template template/java
-build scala-template template/scala
-build python-template template/python
+while getopts u:a:f: flag
+do
+    case "${flag}" in
+        u) username=${OPTARG};;
+        *);;
+    esac
+done
+
+if [ "$username" == "" ] ; then
+  echo "Invalid user name"
+  exit
+fi
+
+build "$username" spark base
+build "$username" spark master
+build "$username" spark worker
+build "$username" spark submit
+build "$username" spark java-template template/java
+build "$username" spark scala-template template/scala
+build "$username" spark python-template template/python
