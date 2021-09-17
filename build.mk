1896b982a022d68d3c0388eb8f5ef708a511b18c
Rogerio Machado <rogermm@Rogerios-iMac-Pro.local>
2021-09-17 10:54:47 -0300

    Update

---
+++ build.mk
@@ -0,0 +1,6 @@
+Prj([Docker(name="base",        prefix="spark"),
+     Docker(name="base-python", prefix="spark"),
+     Docker(name="master",      prefix="spark"),
+     Docker(name="worker",      prefix="spark"),
+     Docker(name="submit",      prefix="spark")],
+     "docker-spark")
